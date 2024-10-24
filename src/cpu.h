047a7cbdde33306eeceeba950907149b8c40e825
peppino notaro <peppenotaro05@gmail.com>
2024-10-24 21:39:16 +0200

    cpu.h - Header file for CPU simulator

--- src/cpu.h
+++ src/cpu.h
@@ -1,2 +1,19 @@
-// CPU structure and declarations
+// cpu.h - Header file for CPU simulator
 
+#ifndef CPU_H
+#define CPU_H
+
+// Structure representing the CPU
+struct CPU {
+    int R1, R2, R3;        
+    int memory[50];       
+    int programCounter;    
+};
+
+// Function declarations
+void initializeCPU(struct CPU *cpu);
+void executeInstruction(struct CPU *cpu, char *instruction);
+void printState(struct CPU *cpu);
+void executeProgram(struct CPU *cpu, char *program[], int programLength);
+
+#endif
